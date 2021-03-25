754ae0fa04c019098f488e522d95a6c5291a544e
Edgar Arevalo <edgarevalo1990@gmail.com>
2021-03-25 18:13:09 -0300

    declaracion de variables actualizada

--- Infra/init.tf
+++ Infra/init.tf
@@ -1,5 +1,11 @@
 provider "aws" {
   region = var.aws_region
-  access_key = 
-  secret_key = 
+  access_key = "AKIAUQWA55TCIWCZDW6F"
+  secret_key = "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"
+}
+
+######## VARIABLES #########
+
+variable "aws_region" {
+    type = string
 }

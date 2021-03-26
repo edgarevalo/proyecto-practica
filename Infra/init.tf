f8f6025bc3e110dc03fa5fac06765f6ee18a3114
Edgar Arevalo <edgarevalo1990@gmail.com>
2021-03-26 10:57:56 -0300

    cambio de modo de autenticación, se conecta con Vault para obtener las credenciales de AWS

--- Infra/init.tf
+++ Infra/init.tf
@@ -1,8 +1,33 @@
-provider "aws" {
-  region = var.aws_region
-  access_key = ""
-  secret_key = ""
+variable "vault_aws_backend" {
+  type = string
+  description = "path to aws secrets engine"
+  default = "aws"
 }
+
+variable "vault_aws_role"{
+  type = string
+  description = "name of aws role for credentials"
+  default = "edgar-vault"
+}
+
+provider "vault" {
+  #VAULT_ADDR Y VAULT_TOKEN seteados como variables de entorno
+}
+
+provider "aws"{
+  region = var.aws_region
+}
+
+data "vault_generic_secret" "appkey" {
+  path = "secret/appkey"
+}
+
+data "vault_aws_access_credentials" "creds" {
+  backend = var.vault_aws_backend
+  role = var.vault_aws_role
+  type = "sts"
+}
+
 
 ######## VARIABLES #########
 
